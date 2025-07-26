ac99e9a98ede7fe450905eaeb413e3735cf9b83a
Sapper <saniamykhailenko@gmail.com>
2025-07-26 13:19:29 +0300

    initial repo and start

---
+++ src/js/products-api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { API_BASE_URL, API_ENDPOINTS, ITEMS_PER_PAGE } from './constants';
+
+axios.defaults.baseURL = API_BASE_URL;
+
+export async function getCategories() {
+    const results = await axios(`${API_ENDPOINTS.CATEGORIES}`);
+
+    return results.data;
+}
+
+export async function getProducts(currentPage) {
+    const skip = (currentPage - 1) * ITEMS_PER_PAGE;
+
+    const { data } = await axios(`?limit=${ITEMS_PER_PAGE}&skip=${skip}`);
+    return data;
+}
